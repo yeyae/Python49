004ed1550eef2627d6130983dec7ba5caf811d64
yeyae <whyjinny@naver.com>
2022-09-19 09:38:21 +0900

    python

---
+++ 49.py
@@ -0,0 +1,11 @@
+class FourCal:
+    def setdata(self, first, second):
+        self.first = first
+        self.second = second
+    def add(self):
+        result = self.first + self.second
+        return result        
+#더하기 기능 만들기
+a = FourCal()
+a.setdata(4, 2)
+print(a.add())
