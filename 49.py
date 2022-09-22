c95ac93c309cf53a16dbdd15304ee0b741e48552
JINNY <103866627+yeyae@users.noreply.github.com>
2022-09-22 09:31:14 +0900

    Update 49.py

--- 49.py
+++ 49.py
@@ -15,24 +15,22 @@ class FourCal:
     def setdata(self, first, second):
         self.first = first
         self.second = second
-    def add(self):
- result = self.first * self.second
-        return result        
- result = self.first - self.second
-        return result        
- result = self.first / self.second
-        return result        
+    def mul(self):
+        result = self.first * self.second
+        return result      
 
-#기능들 결과 뽑아내기
-a = FourCal()
-b = FourCal()
-a.setdata(4, 2)
-b.setdata(3, 8)
-print(a.add())
-print(a.mul())
-print(a.sub())
-print(a.div())
-print(b.add())
-print(b.mul())
-print(b.sub())
-print(b.div())
+class FourCal:
+    def setdata(self, first, second):
+        self.first = first
+        self.second = second
+    def sub(self):
+        result = self.first - self.second
+        return result      
+
+class FourCal:
+    def setdata(self, first, second):
+        self.first = first
+        self.second = second
+    def div(self):
+        result = self.first / self.second
+        return result      
